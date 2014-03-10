ce85957ecc3ed08e17c25ae6efdd435485aee27e
Will Canine <willcanine@gmail.com>
2014-03-10 17:32:18 -0400

    added move recording in JSON format

--- gen/com/willcanine/midtermirlchessstart/R.java
+++ gen/com/willcanine/midtermirlchessstart/R.java
@@ -25,18 +25,19 @@ public final class R {
         public static final int logo1=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000b;
-        public static final int end_game_button=0x7f080005;
+        public static final int action_settings=0x7f08000c;
+        public static final int black_wins_button=0x7f080006;
         public static final int enter_move_button=0x7f080004;
         public static final int enter_move_field=0x7f080003;
-        public static final int event_field=0x7f080006;
+        public static final int event_field=0x7f080007;
         public static final int imageView1=0x7f080000;
-        public static final int location_field=0x7f080007;
-        public static final int playing_black_field=0x7f080009;
-        public static final int playing_white_field=0x7f080008;
-        public static final int save_button=0x7f08000a;
+        public static final int location_field=0x7f080008;
+        public static final int playing_black_field=0x7f08000a;
+        public static final int playing_white_field=0x7f080009;
+        public static final int save_button=0x7f08000b;
         public static final int start_button=0x7f080001;
         public static final int turn_indicator=0x7f080002;
+        public static final int white_wins_button=0x7f080005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -52,6 +53,7 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int black_to_play=0x7f05000f;
+        public static final int black_wins=0x7f050011;
         public static final int end_game=0x7f05000d;
         public static final int enter_move=0x7f05000c;
         public static final int event=0x7f050007;
@@ -65,6 +67,7 @@ public final class R {
         public static final int title_activity_play_game=0x7f05000b;
         public static final int title_activity_start_game=0x7f050004;
         public static final int white_to_play=0x7f05000e;
+        public static final int white_wins=0x7f050010;
     }
     public static final class style {
         /** 
